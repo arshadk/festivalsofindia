bbb21c15a18c690526094376f226da06ed8841f5
arshadk <arshad.a.k@gmail.com>
2016-06-30 11:01:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 var routes= require("./routes/routes.js")(app);
 
-var server= app.listen(3000,function(){
+var server= app.listen(80,function(){
     console.log("listening at port %s...",server.address().port)
 });
 
